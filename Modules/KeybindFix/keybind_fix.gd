ce0df923d68ee02a561a6579a63806eada8ca91c
K <65287118+katzerax@users.noreply.github.com>
2024-11-14 22:47:38 -0800

    are they stupid

--- Modules/KeybindFix/keybind_fix.gd
+++ Modules/KeybindFix/keybind_fix.gd
@@ -1,20 +1,34 @@
 extends Node
+
 var prefix = "[KMOD.keybind_fix] "
+var previous_scene_name = ""
+var has_executed_menu_action = false
 
 func _ready():
-	get_tree().connect("scene_changed", self, "_on_scene_changed")
-	print(prefix, "Connected to scene_changed signal.")
+	print(prefix, "Initialization complete. Starting scene check.")
+	set_process(true)
 
-func _on_scene_changed():
-	var current_scene_name = get_tree().current_scene.name
-
-func yield_until_not_splash():
-	while get_tree().current_scene.name == "splash":
-		yield(get_tree(), "idle_frame")
-	print(prefix, "Scene is no longer splash")
-	_open_and_close_options_menu()
+func _process(delta):
+	if has_executed_menu_action:
+		set_process(false)
+		return
+	var current_scene = get_tree().current_scene
+	var current_scene_name = ""
+	if current_scene != null:
+		current_scene_name = current_scene.name
+	else:
+		current_scene_name = "null"
+	if current_scene_name != previous_scene_name:
+		print(prefix, "Scene changed to: ", current_scene_name)
+		previous_scene_name = current_scene_name
+		if current_scene_name != "splash" and current_scene_name != "null":
+			_open_and_close_options_menu()
+			has_executed_menu_action = true
 
 func _open_and_close_options_menu():
-	OptionsMenu._open()
-	OptionsMenu._close()
-	print(prefix, "Executed OptionsMenu _open & _close functions.")
+	if typeof(OptionsMenu) == TYPE_OBJECT and OptionsMenu.has_method("_open") and OptionsMenu.has_method("_close"):
+		OptionsMenu._open()
+		OptionsMenu._close()
+		print(prefix, "Executed OptionsMenu _open & _close functions.")
+	else:
+		print(prefix, "OptionsMenu not found or does not have required methods.")
